1e896bc5f8a92c7ac8fd10348871606a940ac1f6
Lukas Mikuta <lukasenokmikuta@gmail.com>
2022-10-12 11:16:49 +0200

    Commit inden netlify

--- src/facilities.js
+++ src/facilities.js
@@ -2,7 +2,7 @@
 let facilities = function(facilities) {
 
     let element = document.createElement('section')
-    element.classList.add('makingSec')
+    element.classList.add('makingSection')
     
     element.innerHTML = `
             <h1 class="">${facilities.headline}</h1>
@@ -10,8 +10,9 @@ let facilities = function(facilities) {
             <img src="${facilities.icon}" alt="">
             <h2 class="">${facilities.headline}</h2>
             <p class="">${facilities.text}</p>
-            <p><a href="">${facilities}</a></p>
+            <p><a href="#">Show me more</a></p>
         </div>
+
     `
     
     return element
